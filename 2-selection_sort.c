184dd644ba36be0072dc8c69605b08be30278871
facu2279 <2279@holbertonschool.com>
2021-02-01 20:03:47 +0000

    Add task 2 with betty fixed

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -4,6 +4,38 @@
  * Date - 01/02/2021
  */
 #include "sort.h"
+#include <string.h>
 /**
-*
-*/
+ * selection_sort - sorts
+ * @array: array
+ * @size: size of array
+ */
+void selection_sort(int *array, size_t size)
+{
+	unsigned int i = 0, a = 0, swap = 0, count = 0, menor = 0;
+	int min = 0;
+
+	while (i < size)
+	{
+		count = 0;
+		a = i;
+		min = array[i];
+		for (; a < size; a++)
+		{
+			if (min > array[a])
+			{
+				min = array[a];
+				menor = a;
+				count = 1;
+			}
+		}
+		if (count == 1)
+		{
+			swap = array[i];
+			array[i] = array[menor];
+			array[menor] = swap;
+			print_array(array, size);
+		}
+		i++;
+	}
+}
--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -7,3 +7,19 @@
 /**
 *
 */
+
+int _strcmp(int *s1, int *s2)
+{
+int i, len, len2;
+
+len = strlen(s1);
+len2 = strlen(s2);
+for (i = 0; i < len && i < len2; i++)
+{
+if (s1[i] != s2[i])
+{
+return (1);
+}
+}
+return (0);
+}
