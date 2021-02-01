81ffe42ee7b9b029e12d8730af1de14c1ed9715b
facu2279 <2279@holbertonschool.com>
2021-02-01 16:36:00 +0000

    Add task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,3 +7,41 @@
 /**
 *
 */
+int estaordeanda(int *array, int size);
+void bubble_sort(int *array, size_t size)
+{
+    int aux;
+    unsigned int i = 0;
+
+    while (estaordeanda(array, size) == 0)
+    {
+        i = 0;
+        while(i < size - 1)
+        {
+            
+            if (array[i] > array[i + 1])
+            {
+                aux = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = aux;
+                print_array(array, size);
+            }
+        i++;
+        }
+    }
+}
+
+int estaordeanda(int *array, int size)
+{
+    int i = 0;
+
+    while(i < size - 1)
+    {
+        if (array[i] > array[i + 1])
+        {
+            return (0);
+        }
+        i++;
+    }
+    return (1);
+}
